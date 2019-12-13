22673af0a804b6981a0e606f8808937b03b1af68
Omer <omkomilko3@gmail.com>
2019-12-13 11:26:02 +0100

    boardlist now shows the created board, create list component creates a list within that board, show list component shows the list created within that board

--- src/components/BoardDetail.js
+++ src/components/BoardDetail.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getBoards } from "../actions";
+import CreateList from "./CreateList";
+import ShowLists from "./ShowLists";
 
-const BoardDetail = () => {
-	return <div>BoardDetail</div>;
+const BoardDetail = props => {
+	useEffect(() => {
+		props.getBoards();
+	}, []);
+
+	const renderComponent = () => {
+		if (props.boards) {
+			const obj = props.boards.find(obj => obj.id == props.match.params.id);
+			if (obj) {
+				return (
+					<div>
+						<h2>{obj.boardName}</h2>
+						<CreateList id={obj.id} />
+						<ShowLists id={obj.id} />
+					</div>
+				);
+			}
+		}
+		return "No board to show";
+	};
+
+	return (
+		<div>
+			<h2>{renderComponent()}</h2>
+		</div>
+	);
 };
 
-export default BoardDetail;
+const mapStateToProps = state => {
+	return { boards: state.boards };
+};
+
+export default connect(mapStateToProps, { getBoards })(BoardDetail);
---
+++ src/components/CreateList.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { createList } from "../actions";
+import { connect } from "react-redux";
+
+const CreateList = props => {
+	const [list, setList] = useState("");
+
+	const onSubmit = e => {
+		e.preventDefault();
+		props.createList(props.id, list);
+		setList("");
+	};
+
+	return (
+		<form onSubmit={onSubmit} className="ui form">
+			<input type="text" onChange={e => setList(e.target.value)} value={list} />
+			<button className="ui button primary" type="submit">
+				Create List
+			</button>
+		</form>
+	);
+};
+
+export default connect(null, { createList })(CreateList);
